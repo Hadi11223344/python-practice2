86a8cc5c969307319e421509dbdb6539b64b3db4
Hammad Tahir <hammadtahirhammadtahir4@gmail.com>
2023-09-14 00:18:44 +0500

    Sort() Method Behavior

---
+++ sort_list.py
@@ -0,0 +1,7 @@
+# calling sort() mutates the list, returns nothing 
+
+warm = ['red', 'blue', 'green', 'yellow']
+sorted_warm = warm.sort()
+print(warm)
+print(sorted_warm) # It prints None as the sort() method mutates the list, returns nothing
+
