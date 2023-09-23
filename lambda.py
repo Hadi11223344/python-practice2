c13100fa708675a435498633a5e725437ca8498a
Hammad <hammadtahirhammadtahir4@gmail.com>
2023-09-23 12:14:42 +0500

    Lambda function 2

--- lambda.py
+++ lambda.py
@@ -9,22 +9,37 @@
 
 # rv1 = (lambda x: x*x )
 
-# print(rv1(5))
+# # print(rv1(5))
 
-# myadd = lambda a, b: a+b
-# mysub = lambda a, b: a-b
-# mymul = lambda a, b: a*b
+# # myadd = lambda a, b: a+b
+# # mysub = lambda a, b: a-b
+# # mymul = lambda a, b: a*b
 
-def mycalc(op, a, b):
-    return op(a,b)
+# def mycalc(op, a, b):
+#     return op(a,b)
 
 
-# rv1 = mycalc(myadd, 8, 2)
-# rv2 = mycalc(mysub, 8, 2)
-# rv3 = mycalc(mymul, 8, 2)
+# # rv1 = mycalc(myadd, 8, 2)
+# # rv2 = mycalc(mysub, 8, 2)
+# # rv3 = mycalc(mymul, 8, 2)
 
-# we can all above code into these three lines of code
-rv1 = mycalc(lambda a, b: a + b, 8, 2)
-rv2 = mycalc(lambda a, b: a - b, 8, 2)
-rv3 = mycalc(lambda a, b: a * b, 8, 2)
-print(rv1 , rv2 , rv3)
+# # we can all above code into these three lines of code
+# rv1 = mycalc(lambda a, b: a + b, 8, 2)
+# rv2 = mycalc(lambda a, b: a - b, 8, 2)
+# rv3 = mycalc(lambda a, b: a * b, 8, 2)
+# print(rv1 , rv2 , rv3)
+from functools import reduce
+
+# numbers = [20, -20, 30, -4, -5]
+
+
+# rv1 = (reduce(lambda a, b: a if a > b else b, numbers, 0))
+
+# print(rv1)
+
+mylist = [ 75 , 86 , 87 , 88 , 89 ]
+map_object = map(lambda num: num%5, mylist)
+remainders = list(map_object)
+
+print("Original list: ", mylist)
+print("List of remainders: ", remainders)
